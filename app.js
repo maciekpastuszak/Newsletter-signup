97aad3572a8cf738aeb19b23fbcc9361b1861fe7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-11-21 09:50:44 +0100

    implement and check first post route

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+const e = require('express');
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", (req,res) => {
+var fName = req.body.fName;
+var lName = req.body.lName;
+var email = req.body.email;
+console.log(fName, lName, email);
+})
 
 app.listen(3000, () => {
     console.log("Slucham na porcie 3000");
