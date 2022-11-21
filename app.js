06abfcfbd7ade5e0c7424b7d4657de0de25e9a91
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-11-21 09:42:20 +0100

    styles update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 
+app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
