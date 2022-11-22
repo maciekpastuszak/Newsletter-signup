0a0865e465ac3c51a392b2c20631de38ac31a0f2
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-11-22 09:30:36 +0100

    setup mailchimp and seed data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
-const e = require('express');
+const https = require ("https");
+
+require('dotenv').config()
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,12 +14,40 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req,res) => {
-var fName = req.body.fName;
-var lName = req.body.lName;
-var email = req.body.email;
-console.log(fName, lName, email);
-})
+const firstName = req.body.fName;
+const lastName = req.body.lName;
+const email = req.body.email;
+
+var data = {
+members:[
+    {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+        }
+    }
+]
+};
+
+let jsonData = JSON.stringify(data);
+let url = `https://us21.api.mailchimp.com/3.0/lists/${process.env.AUDIENCE_ID}`;
+let options = {
+method: "POST",
+auth: `paciek:${process.env.MAILCHIMP_API}`
+};
+
+const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+        console.log(JSON.parse(data));
+    })
+});
+request.write(jsonData);
+request.end();
+});
 
 app.listen(3000, () => {
     console.log("Slucham na porcie 3000");
 })
+
